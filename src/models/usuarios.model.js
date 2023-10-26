308a02908292d368f2ef384f3409d43bf83c1190
Alfredo Carrasquel <alfredocrdev@gmail.com>
2023-10-26 13:18:53 -0300

    Update usuarios.model.js
    se elimino el requerido a todos las llaves

--- src/models/usuarios.model.js
+++ src/models/usuarios.model.js
@@ -3,12 +3,12 @@ const mongoose = require("mongoose");
 const userCollecion = "users"
 
 const userSchema = new mongoose.Schema({
-  first_name: {type: String, max: 30, required: true},
-  last_name: {type: String, max: 30,},
-  email: {type: String, max: 30, required: true},
-  age: {type: Number, required: true},
+  first_name: {type: String, max: 30},
+  last_name: {type: String, max: 30},
+  email: {type: String, max: 30},
+  age: {type: Number},
   password: {type: String, max: 60},
-  rol: {type: String, max: 60, required: true},
+  rol: {type: String, max: 60},
 })
 
 const userModel = mongoose.model(userCollecion, userSchema)
